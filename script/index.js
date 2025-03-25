0ce95afe58b858f9553ab45c37b4ae1b8cf7e2a8
Ana Vitória Nascimento Lopes <anavitorianlopes@gmail.com>
2025-03-25 19:04:05 +0000

    Customer Page

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var mapElement = document.querySelector(".map");
+  var mapElement = document.querySelector(".content__map");
 
   if (!mapElement) {
     console.error("Elemento do mapa não encontrado!");
@@ -23,28 +23,28 @@ document.addEventListener("DOMContentLoaded", function () {
       country: "England",
       coords: [51.5074, -0.1278],
       description: "Capital of England",
-      photo: "img/london.jpg",
+
       link: "https://en.wikipedia.org/wiki/London",
     },
     {
       country: "Ireland",
       coords: [53.3498, -6.2603],
       description: "Capital of Ireland",
-      photo: "img/dublin.jpg",
+
       link: "https://en.wikipedia.org/wiki/Dublin",
     },
     {
       country: "Australia",
       coords: [-35.2809, 149.13],
       description: "Capital of Australia",
-      photo: "img/canberra.jpg",
+
       link: "https://en.wikipedia.org/wiki/Canberra",
     },
     {
       country: "Czech Republic",
       coords: [50.0755, 14.4378],
       description: "Capital of the Czech Republic",
-      photo: "img/prague.jpg",
+
       link: "https://en.wikipedia.org/wiki/Prague",
     },
     {
@@ -53,12 +53,13 @@ document.addEventListener("DOMContentLoaded", function () {
       description: "Capital of Denmark",
       photo: "img/copenhagen.jpg",
       link: "https://en.wikipedia.org/wiki/Copenhagen",
+      imageClass: "client-image",
     },
     {
       country: "Finland",
       coords: [60.1699, 24.9384],
       description: "Capital of Finland",
-      photo: "img/helsinki.jpg",
+
       link: "https://en.wikipedia.org/wiki/Helsinki",
     },
     {
@@ -67,6 +68,7 @@ document.addEventListener("DOMContentLoaded", function () {
       description: "Capital of France",
       photo: "img/paris.jpg",
       link: "https://en.wikipedia.org/wiki/Paris",
+      imageClass: "client-image",
     },
     {
       country: "Germany",
@@ -74,33 +76,34 @@ document.addEventListener("DOMContentLoaded", function () {
       description: "Capital of Germany",
       photo: "img/berlin.jpg",
       link: "https://en.wikipedia.org/wiki/Berlin",
+      imageClass: "client-image",
     },
     {
       country: "Netherlands",
       coords: [52.3676, 4.9041],
       description: "Capital of the Netherlands",
-      photo: "img/amsterdam.jpg",
+
       link: "https://en.wikipedia.org/wiki/Amsterdam",
     },
     {
       country: "Norway",
       coords: [59.9139, 10.7522],
       description: "Capital of Norway",
-      photo: "img/oslo.jpg",
+
       link: "https://en.wikipedia.org/wiki/Oslo",
     },
     {
       country: "Poland",
       coords: [52.2298, 21.0118],
-      description: "Capital of Poland",
-      photo: "img/warsaw.jpg",
-      link: "https://en.wikipedia.org/wiki/Warsaw",
+      description: "Our Clients in Poland",
+
+      link: "http://127.0.0.1:5501/polandClients.html",
     },
     {
       country: "Romania",
       coords: [44.4268, 26.1025],
       description: "Capital of Romania",
-      photo: "img/bucharest.jpg",
+
       link: "https://en.wikipedia.org/wiki/Bucharest",
     },
     {
@@ -109,76 +112,78 @@ document.addEventListener("DOMContentLoaded", function () {
       description: "Capital of Russia",
       photo: "img/moscow.jpg",
       link: "https://en.wikipedia.org/wiki/Moscow",
+      imageClass: "client-image",
     },
     {
       country: "Slovenia",
       coords: [46.0511, 14.5051],
       description: "Capital of Slovenia",
-      photo: "img/ljubljana.jpg",
+
       link: "https://en.wikipedia.org/wiki/Ljubljana",
     },
     {
       country: "Ukraine",
       coords: [50.4501, 30.5236],
       description: "Capital of Ukraine",
-      photo: "img/kiev.jpg",
+
       link: "https://en.wikipedia.org/wiki/Kiev",
     },
     {
       country: "South Africa",
       coords: [-25.746, 28.1881],
       description: "Capital of South Africa",
-      photo: "img/pretoria.jpg",
+
       link: "https://en.wikipedia.org/wiki/Pretoria",
     },
     {
       country: "Brazil",
       coords: [-15.7801, -47.9292],
       description: "Capital of Brazil",
-      photo: "img/brasilia.jpg",
+
       link: "https://en.wikipedia.org/wiki/Brasília",
     },
     {
       country: "Ecuador",
       coords: [-0.1807, -78.4678],
       description: "Capital of Ecuador",
-      photo: "img/quito.jpg",
+
       link: "https://en.wikipedia.org/wiki/Quito",
     },
     {
       country: "Mexico",
       coords: [19.4326, -99.1332],
       description: "Capital of Mexico",
-      photo: "img/mexico-city.jpg",
+
       link: "https://en.wikipedia.org/wiki/Mexico_City",
     },
     {
       country: "Peru",
       coords: [-12.0464, -77.0428],
       description: "Capital of Peru",
-      photo: "img/lima.jpg",
+
       link: "https://en.wikipedia.org/wiki/Lima",
     },
     {
       country: "Bangladesh",
       coords: [23.8103, 90.4125],
       description: "Capital of Bangladesh",
-      photo: "img/dhaka.jpg",
+
       link: "https://en.wikipedia.org/wiki/Dhaka",
     },
     {
       country: "India",
       coords: [28.6139, 77.209],
       description: "Capital of India",
-      photo: "img/new-delhi.jpg",
+
       link: "https://en.wikipedia.org/wiki/New_Delhi",
     },
     {
       country: "Indonesia",
       coords: [-6.1751, 106.865],
       description: "Capital of Indonesia",
-      photo: "img/jakarta.jpg",
+
       link: "https://en.wikipedia.org/wiki/Jakarta",
+      imageClass: "client-image",
     },
     {
       country: "Japan",
@@ -191,21 +196,21 @@ document.addEventListener("DOMContentLoaded", function () {
       country: "South Korea",
       coords: [37.5665, 126.978],
       description: "Capital of South Korea",
-      photo: "img/seoul.jpg",
+
       link: "https://en.wikipedia.org/wiki/Seoul",
     },
     {
       country: "Pakistan",
       coords: [33.6844, 73.0479],
       description: "Capital of Pakistan",
-      photo: "img/islamabad.jpg",
+
       link: "https://en.wikipedia.org/wiki/Islamabad",
     },
     {
       country: "Philippines",
       coords: [14.5995, 120.9842],
       description: "Capital of Philippines",
-      photo: "img/manila.jpg",
+
       link: "https://en.wikipedia.org/wiki/Manila",
     },
     {
@@ -214,19 +219,20 @@ document.addEventListener("DOMContentLoaded", function () {
       description: "Capital of Taiwan",
       photo: "img/taipei.jpg",
       link: "https://en.wikipedia.org/wiki/Taipei",
+      imageClass: "client-image",
     },
     {
       country: "Thailand",
       coords: [13.7563, 100.5018],
       description: "Capital of Thailand",
-      photo: "img/bangkok.jpg",
+
       link: "https://en.wikipedia.org/wiki/Bangkok",
     },
     {
       country: "Vietnam",
       coords: [21.0285, 105.8542],
       description: "Capital of Vietnam",
-      photo: "img/hanoi.jpg",
+
       link: "https://en.wikipedia.org/wiki/Hanoi",
     },
   ];
@@ -247,12 +253,11 @@ document.addEventListener("DOMContentLoaded", function () {
   // Adiciona os pins e popups para cada país com o ícone customizado
   locations.forEach((location) => {
     var popupContent = `
-  <div class="popup-content">
-    <img src="${location.photo}" alt="${location.country}" class="popup-img"/>
-    <p><strong>${location.country}</strong></p>
-    <p>${location.description}</p>
-    <a href="${location.link}" target="_blank" class="popup-link">Learn more</a>
-  </div>
+    <div class="popup-content">
+      <h3>${location.country}</h3>
+      <p>${location.description}</p>
+      <a href="${location.link}" target="_blank">Saiba mais</a>
+    </div>
   `;
 
     L.marker(location.coords, { icon: customIcon })
@@ -260,3 +265,43 @@ document.addEventListener("DOMContentLoaded", function () {
       .bindPopup(popupContent);
   });
 });
+//////////////////////////////////
+
+// Add event listener for changes to the second dropdown (Unit)
+document.getElementById("dropdown1").addEventListener("change", autoSearch);
+document.getElementById("dropdown2").addEventListener("change", autoSearch);
+
+function autoSearch() {
+  const dropdown1 = document.getElementById("dropdown1").value;
+  const dropdown2 = document.getElementById("dropdown2").value;
+  const resultDiv = document.getElementById("result");
+
+  // Check if both dropdowns have selections
+  if (!dropdown1 || !dropdown2) {
+    resultDiv.innerHTML =
+      '<p class="search__error">Please select both options.</p>';
+    return;
+  }
+
+  // Variables for description and link based on selected options
+  let description = "";
+  let link = "";
+
+  // Assign values based on the combination of dropdown selections
+  if (dropdown1 === "Ruchenna Duck" && dropdown2 === "Unit 1") {
+    description = "This is a brief description for Ruchenna Duck in Unit 1.";
+    link = "http://127.0.0.1:5501/monitoringSystem.html";
+  } else if (dropdown1 === "Avibio Plus" && dropdown2 === "Unit 1") {
+    description = "This is a brief description for Avibio Plus in Unit 1.";
+    link = "https://example.com/avibio-plus"; // Substitua pelo link real
+  }
+
+  // Create the result card with description and link
+  resultDiv.innerHTML = `
+      <div class="search__result-box">
+        <h3 class="search__title">${dropdown1}</h3>
+        <p>${description}</p>
+        <a href="${link}" target="_blank" class="search__link">View Details</a>
+      </div>
+    `;
+}
